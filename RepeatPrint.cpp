b3fe8c6c09003d1da788088bf729f5685db30645
sanja0508 <pawarsanjana2006@gmail.com>
2025-08-07 15:44:29 +0530

    Create RepeatPrint.cpp

---
+++ RepeatPrint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Printing 'SIT' 5 times:\n\n";
+
+    for (int i = 1; i <= 5; i++) {
+        cout << "SIT\n";
+    }
+
+    cout << "\nDone printing! \n";
+    return 0;
+}
